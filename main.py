7d193c821b31c8cb525b4f06840099bb19347c61
dkooij <d.kooij-1@student.utwente.nl>
2020-10-15 16:58:28 +0200

    Load images

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello")
+import h5py
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.spatial.distance import squareform
+from skimage.color import rgb2gray
+from skimage.feature import ORB
+
+# Load image file names
+with open("data/W17/W17.json") as f:
+    data = json.load(f)
+    im_paths = data["im_paths"]
+    poses = np.array(data["poses"])
+
+# Load similarity matrix
+with h5py.File("data/W17/W17_similarity.h5", "r") as f:
+    gt_labels = squareform(f["sim"][:].flatten())
+
+im_paths = im_paths[:10]  # TODO: Limit to only first 10 images
+
+# Load images
+descriptor_extractor = ORB(n_keypoints=200)
+descriptors = []
+for im_path in im_paths:
+    img = plt.imread("data/" + im_path)
+    img = rgb2gray(img)
+    descriptor_extractor.detect_and_extract(img)
+    descriptors.extend(descriptor_extractor.descriptors)
+descriptors = np.array(descriptors)
